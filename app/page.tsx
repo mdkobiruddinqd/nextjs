814007863c5d167c279f6fb0738dd86138bbc918
Kobir <kobir.uddin@qdata.nyc>
2023-08-23 01:02:36 +0600

    pages

--- app/page.tsx
+++ app/page.tsx
@@ -100,13 +100,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            AU Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            KU --- Instantly deploy your Next.js site to a shareable URL with
+            KU AU--- Instantly deploy your Next.js site to a shareable URL with
             Vercel.
           </p>
         </a>
