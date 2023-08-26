08e7280489e7d9bb295f6bba79cb8da3f706bbd5
mdkobiruddinqd <141666447+mdkobiruddinqd@users.noreply.github.com>
2023-08-26 20:36:22 +0600

    followed tutorials

--- app/page.tsx
+++ app/page.tsx
@@ -16,42 +16,36 @@ export default function Home() {
             </div>
           </div>
         </nav>
-        <main>
-          {/* Header */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]">
-            <div className="text-center">
-              <h1 className="text-white text-5xl font-bold mb-2 pt-12">
-                Find your table for any occasion
-              </h1>
-            </div>
-            <div className="text-left py-3 m-auto flex justify-center">
-              <input
-                className="text-gray-700 bg-gray-300 rounded text-lg py-2 px-5 w-[450px]"
-                type="text"
-                placeholder={"State, City or Town"}
-              ></input>
-              <button className="btn btn-error ml-3">Let's go</button>
-            </div>
-          </div>
-          {/* /Header */}
-        </main>
-        {/* cards */}
-        <div className="flex flex-wrap py-3 px-36 mt-10">
-          <div className="bg-gray-800 w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-            <img src="img-2.webp" alt="" className="w-full h-36"></img>
 
-            <div className="py-3 px-3">
-              <h3 className="text-blue-100 font-bold text-md">
-                Milestones Grill
-              </h3>
-              <div className="font-bold flex items-start pt-1">
-                <p className="text-xl text-orange-500">*****</p>
-                <p className="text-sm ml-3 text-gray-400">77 reviews</p>
-              </div>
-            </div>
+        {/* Header */}
+
+        <div className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full">
+            <h1 className="text-5xl text-white text-shadow text-center ">
+              Milestones Grill (New York)
+            </h1>
           </div>
         </div>
-        {/* cards */}
+        {/* Description */}
+        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-9">
+          <div className="bg-white w-[70%] rounded p-3 shadow">
+            {/* Restaurant Navbar */}
+            <nav className="flex text-md border-b pb-2 text-slate-600">
+              <a href="#" className="mr-7">
+                Overview
+              </a>
+              <a href="#" className="mr-7">
+                Menu
+              </a>
+              <a href="#" className="mr-7">
+                Photos
+              </a>
+              <a href="#" className="mr-7">
+                Contact
+              </a>
+            </nav>
+          </div>
+        </div>
       </main>
     </main>
   );
